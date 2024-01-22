5387028a994469562b1d565343075c49903fb3c2
Elan456 <telan4892@gmail.com>
2024-01-22 18:00:23 -0500

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.title("Ethan Anderson's Website Test")
+    # Add your resume content using Streamlit components
+
+
+if __name__ == "__main__":
+    main()
